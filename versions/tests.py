8a51d70da28811b024e3a44563a3c16029c61b9b
Ahtii <ahtishamshafi9906@gmail.com>
2023-08-20 23:20:55 +0530

    add test case for python version get

--- versions/tests.py
+++ versions/tests.py
@@ -10,6 +10,38 @@ from versions.models import PythonVersion
 from versions.serializers import DjangoVersionSerializer
 
 
+from django.test import TestCase
+from django.shortcuts import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from versions.models import PythonVersion
+from versions.models import DjangoVersion
+
+from versions.serializers import PythonVersionSerializer
+from versions.serializers import DjangoVersionSerializer
+
+
+class PythonVersionTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.python_version = PythonVersion.objects.create(
+            version_id="3.8.0",
+            release_date="2019-10-14",
+            end_of_life_date="2024-04-10",
+            is_supported=True,
+            doc_url="https://www.python.org/downloads/release/python-380/"
+        )
+        self.endpoint_url = reverse('python_version', kwargs={'pk': self.python_version.pk})
+
+    def test_response(self):
+        response = self.client.get(self.endpoint_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, PythonVersionSerializer(self.python_version).data)
+
+
 class DjangoVersionTestCase(TestCase):
 
     def setUp(self):
